b94addd4d4edb82848807d02cabceada0d6db635
Phillip <Phillip@PhillipBremer.lan>
2016-02-24 22:31:24 -0600

    Added a PrintReceipt class to actually print the receipt.

---
+++ src/psbdiscountstrategy/PrintReceipt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psbdiscountstrategy;
+
+/**
+ *
+ * @author Phillip Bremer
+ */
+public class PrintReceipt {
+    private Product prodName;
+    private Product prodId;
+    
+    public final void writeReceipt(){
+        //needs validation
+        
+    }
+}
